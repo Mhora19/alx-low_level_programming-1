ba843f04c7b915b46aae74a9d1094c02118d37b3
Gabriel Abayomi Adesanya <110470758+Adesanya07@users.noreply.github.com>
2022-09-08 21:14:27 +0000

    Update 4-puts.c
    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- *main -prints out a string to stdout.
- *Description: using the main function
- *this program prints "Programming is like building a multilingual puzzle
- * return:0 if success
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
