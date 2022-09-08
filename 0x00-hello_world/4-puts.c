c17248af1bf01489e0a91360443c2fddb3c0cd9f
Adesanya07 <Gadesanya22@gmail.com>
2022-09-08 14:16:20 -0700

    correct one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- *main -prints out a string to stdout.
- *Description: using the main function
- *this program prints "Programming is like building a multilingual puzzle
- * return:0 if success
+ * main	-print the phrase "Programming is like building a multilingual puzzle
+ * return (0) success
  */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+int main (void)
+{ 
+	puts("/"Programming is like building a multilingual puzzle");
+	ruturn (0);
 }
